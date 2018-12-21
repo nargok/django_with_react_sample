0ebb8333edff5adc666604e5fba57cfb72d95137
Ryoichi Gokan <ryoichi.gokan@compass-e.com>
2018-12-21 17:50:50 +0900

    Added user_id in UserSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
 
   class Meta:
     model = User
-    fields = ('username',)
+    fields = ('id', 'username',)
 
 class UserSerializerWithToken(serializers.ModelSerializer):
 
